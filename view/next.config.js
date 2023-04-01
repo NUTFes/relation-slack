d3023167c26000f2c548e6ae3fef52c5de93de7e
imaimai17468 <tosiki_801_life@outlook.jp>
2023-04-01 21:39:29 +0900

    [fix] 設定ファイル群を修正

--- view/next.config.js
+++ view/next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NEXT_PUBLIC_APP_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SSR_API_URI: isProd ? 'https://slackbot.nutfes.relation.net' : 'http://api:1323',
+    CSR_API_URI: isProd ? 'https://slackbot.nutfes.relation.net' : 'http://localhost:1323',
+  },
 }
 
 module.exports = nextConfig
