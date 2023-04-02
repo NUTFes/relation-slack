fa3ae48ff01c58d6b9d87c46ff7185576ea1b3db
imaimai17468 <tosiki_801_life@outlook.jp>
2023-04-03 02:23:16 +0900

    [fix] add basic auth

--- view/src/middleware.ts
+++ view/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: ['/', '/index'],
+  matcher: ['/'],
 }
 
 export function middleware(req: NextRequest) {
@@ -12,7 +12,7 @@ export function middleware(req: NextRequest) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
 
-    if (user === '4dmin' && pwd === 'testpwd123') {
+    if (user === process.env.NEXT_PUBLIC_USER && pwd === process.env.NEXT_PUBLIC_PASS) {
       return NextResponse.next()
     }
   }
